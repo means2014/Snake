9a56b54f643553498d5823cb289f345b6385e8b4
Christopher Means <means2014@gmail.com>
2020-05-21 21:12:34 -0400

    Controller code working, score is displayed after losing, gives option to run again

--- snake.py
+++ snake.py
@@ -1,4 +1,9 @@
 import random
+import os
+import pygame
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
 
 def init_board(width, height):
     board = [0]*width
@@ -20,13 +25,13 @@ def random_pos(cols, rows):
 def random_direction():
     return random.choice(['up', 'down', 'left', 'right'])
         
-class snake:
+class Snake:
     def __init__(self, head_position, direction):
         self.length = 1
         self.head = head_position
         self.direction = direction
-        self.body = []
         self.set_velocity()
+        self.body = [[self.head[0]-self.velocity[0], self.head[1]-self.velocity[1]]]
         
     def set_velocity(self):
         if self.direction == 'up':
@@ -38,62 +43,85 @@ class snake:
         elif self.direction == 'left':
             self.velocity = [-1,0]
         
-    def step(self, food = False):
+    def step(self, food = None):
         self.body.insert(0, self.head)
         self.head = [self.head[0]+self.velocity[0], self.head[1]+self.velocity[1]]
-        if not food:
+        
+        if self.head[0] == -1:
+            print("left wall")
+            return -1
+        elif self.head[0] == NUM_COLS: 
+            print("right wall")
+            return -1
+        elif self.head[1] == -1:
+            print("top wall")
+            return -1
+        elif self.head[1] == NUM_ROWS:
+            print("bottom wall")
+            return -1
+        elif self.head in self.body:
+            print("hit self")
+            return -1
+        
+        if self.head != food:
             self.body.pop()
+            return 0
+        return 1
             
     def turn(self, direction):
+        if direction == self.direction:
+            return
+        if direction == 'left' and self.direction == 'right':
+            return
+        if direction == 'right' and self.direction == 'left':
+            return
+        if direction == 'up' and self.direction == 'down':
+            return
+        if direction == 'down' and self.direction == 'up':
+            return
         self.direction = direction
         self.set_velocity()
         
     def draw(self, screen):
         #Draw head
-        HEAD_WIDTH = 0.6
-        EYE_RADIUS = 0.1
-        if self.direction == 'down':
-            pygame.draw.ellipse(screen, (0, 180, 60), (CELL_SIZE[0]*(self.head[0]+(1-HEAD_WIDTH)), CELL_SIZE[1]*self.head[1], CELL_SIZE[0]*HEAD_WIDTH, CELL_SIZE[1]), 0)
-            
-            c1x = CELL_SIZE[0]*(self.head[0]+0.4)
-            c2x = CELL_SIZE[0]*(self.head[0]+0.6)
-            c1y = CELL_SIZE[1]*(self.head[1]+0.8)
-            c2y = CELL_SIZE[1]*(self.head[1]+0.8)
-            
-        elif self.direction == 'up':
-            pygame.draw.ellipse(screen, (0, 180, 60), (CELL_SIZE[0]*(self.head[0]+(1-HEAD_WIDTH)), CELL_SIZE[1]*self.head[1], CELL_SIZE[0]*HEAD_WIDTH, CELL_SIZE[1]), 0)
-            
-            c1x = CELL_SIZE[0]*(self.head[0]+0.4)
-            c2x = CELL_SIZE[0]*(self.head[0]+0.6)
-            c1y = CELL_SIZE[1]*(self.head[1]+0.2)
-            c2y = CELL_SIZE[1]*(self.head[1]+0.2)
-            
-        elif self.direction == 'left':
-            pygame.draw.ellipse(screen, (0, 180, 60), (CELL_SIZE[0]*self.head[0], CELL_SIZE[1]*(self.head[1]+(1-HEAD_WIDTH)), CELL_SIZE[0], CELL_SIZE[1]*HEAD_WIDTH), 0)
-            
-            c1x = CELL_SIZE[0]*(self.head[0]+0.2)
-            c2x = CELL_SIZE[0]*(self.head[0]+0.2)
-            c1y = CELL_SIZE[1]*(self.head[1]+0.4)
-            c2y = CELL_SIZE[1]*(self.head[1]+0.6)
-            
-        else:
-            pygame.draw.ellipse(screen, (0, 180, 60), (CELL_SIZE[0]*self.head[0], CELL_SIZE[1]*(self.head[1]+(1-HEAD_WIDTH)), CELL_SIZE[0], CELL_SIZE[1]*HEAD_WIDTH), 0)
-            
-            c1x = CELL_SIZE[0]*(self.head[0]+0.8)
-            c2x = CELL_SIZE[0]*(self.head[0]+0.8)
-            c1y = CELL_SIZE[1]*(self.head[1]+0.4)
-            c2y = CELL_SIZE[1]*(self.head[1]+0.6)
-            
-        pygame.draw.circle(screen, (255,255,255), (c1x, c1y), EYE_RADIUS, 0)
-        pygame.draw.circle(screen, (0,0,0), (c1x, c1y), EYE_RADIUS/10, 0)
-            
-        pygame.draw.circle(screen, (255,255,255), (c2x, c2y), EYE_RADIUS, 0)
-        pygame.draw.circle(screen, (0,0,0), (c2x, c2y), EYE_RADIUS/10, 0)
+        pygame.draw.polygon(screen, (0,180,60), (
+            [CELL_SIZE[0]*(self.head[0]+1/2), CELL_SIZE[1]*self.head[1]], 
+            [CELL_SIZE[0]*(self.head[0]+1), CELL_SIZE[1]*(self.head[1]+1/2)], 
+            [CELL_SIZE[0]*(self.head[0]+1/2), CELL_SIZE[1]*(self.head[1]+1)], 
+            [CELL_SIZE[0]*self.head[0], CELL_SIZE[1]*(self.head[1]+1/2)]
+            ), 0)
+        
+        #Draw first link
+        if len(self.body) > 1:
+            if self.body[0][0] == self.body[1][0]:
+                if self.body[1][1] == self.body[0][1]+1:
+                    p1 = [CELL_SIZE[0]*(self.body[1][0]), CELL_SIZE[1]*(self.body[1][1])]
+                    p2 = [CELL_SIZE[0]*(self.body[1][0]+1), CELL_SIZE[1]*(self.body[1][1])]
+                    p3 = [CELL_SIZE[0]*(self.body[0][0]+1/2), CELL_SIZE[1]*(self.body[0][1])]
+                else:
+                    p1 = [CELL_SIZE[0]*(self.body[1][0]), CELL_SIZE[1]*(self.body[1][1]+1)]
+                    p2 = [CELL_SIZE[0]*(self.body[1][0]+1), CELL_SIZE[1]*(self.body[1][1]+1)]
+                    p3 = [CELL_SIZE[0]*(self.body[0][0]+1/2), CELL_SIZE[1]*(self.body[0][1]+1)]
+            elif self.body[0][0] == self.body[1][0]-1:
+                p1 = [CELL_SIZE[0]*(self.body[1][0]), CELL_SIZE[1]*(self.body[1][1])]
+                p2 = [CELL_SIZE[0]*(self.body[1][0]), CELL_SIZE[1]*(self.body[1][1]+1)]
+                p3 = [CELL_SIZE[0]*self.body[0][0], CELL_SIZE[0]*(self.body[0][1]+1/2)]
+            else:
+                p1 = [CELL_SIZE[0]*(self.body[1][0]+1), CELL_SIZE[1]*(self.body[1][1]+1)]
+                p2 = [CELL_SIZE[0]*(self.body[1][0]+1), CELL_SIZE[1]*(self.body[1][1])]
+                p3 = [CELL_SIZE[0]*(self.body[0][0]+1), CELL_SIZE[0]*(self.body[0][1]+1/2)]
+            pygame.draw.polygon(screen, (0,180,60), (p1, p2, p3), 0)
+        
         
         #Draw body
-        for cell in self.body[0:-1]
+        for cell in self.body[1:-1]:
             pygame.draw.rect(screen, (0, 180, 60), (cell[0]*CELL_SIZE[0], cell[1]*CELL_SIZE[1], CELL_SIZE[0], CELL_SIZE[1]), 0)
-            pygame.draw.polygon(screen, (228,255,20), ([CELL_SIZE[0]*(cell[0]+1/2), CELL_SIZE[1]*cell[1]], [CELL_SIZE[0]*cell[0], CELL_SIZE[1]*(cell[1]+1/2)], [CELL_SIZE[0]*(cell[0]+1/2), CELL_SIZE[1]*(cell[1]+1)], [CELL_SIZE[0]*(cell[0] + 1/2), CELL_SIZE[1]*(cell[0]+1/2)]), 1)
+            pygame.draw.polygon(screen, (228,255,20), (
+                [CELL_SIZE[0]*(cell[0]+1/2), CELL_SIZE[1]*cell[1]], 
+                [CELL_SIZE[0]*(cell[0]+1), CELL_SIZE[1]*(cell[1]+1/2)], 
+                [CELL_SIZE[0]*(cell[0]+1/2), CELL_SIZE[1]*(cell[1]+1)], 
+                [CELL_SIZE[0]*cell[0], CELL_SIZE[1]*(cell[1]+1/2)]
+                ), 1)
             
         #Draw Tail
         #Moving Down
@@ -101,12 +129,12 @@ class snake:
                 if self.body[-2][1] == self.body[-1][1]+1:
                     p1 = [CELL_SIZE[0]*self.body[-2][0], CELL_SIZE[1]*self.body[-2][1]]
                     p2 = [CELL_SIZE[0]*(self.body[-2][0]+1), CELL_SIZE[1]*self.body[-2][1]]
-                    p3 = [CELL_SIZE[0]*(self.body[-1][0]+1/2), CELL_SIZE[1]*self.body[-1]]
+                    p3 = [CELL_SIZE[0]*(self.body[-1][0]+1/2), CELL_SIZE[1]*self.body[-1][1]]
         #Moving UP
                 else:
                     p1 = [CELL_SIZE[0]*self.body[-1][0], CELL_SIZE[1]*(self.body[-2][1]+1)]
                     p2 = [CELL_SIZE[0]*(self.body[-2][0]+1), CELL_SIZE[1]*(self.body[-2][1]+1)]
-                    p3 = [CELL_SIZE[0]**(self.body[-1][0]+1/2), CELL_SIZE[1]*(self.body[-1]+1)]
+                    p3 = [CELL_SIZE[0]*(self.body[-1][0]+1/2), CELL_SIZE[1]*(self.body[-1][1]+1)]
         #Moving Left
             elif self.body[-2][0] == self.body[-1][0]-1:
                 p1 = [CELL_SIZE[0]*(self.body[-2][0]+1), CELL_SIZE[1]*self.body[-2][1]]
@@ -115,25 +143,28 @@ class snake:
         #Moving Right
             else:
                 p1 = [CELL_SIZE[0]*self.body[-2][0], CELL_SIZE[1]*self.body[-2][1]]
-                p2 = [CELL_SIZE[0]*self.body[-1][0], CELL_SIZE[1]*(self.body[-2][1]+1)]
+                p2 = [CELL_SIZE[0]*self.body[-2][0], CELL_SIZE[1]*(self.body[-2][1]+1)]
                 p3 = [CELL_SIZE[0]*self.body[-1][0], CELL_SIZE[1]*(self.body[-1][1]+1/2)]
-        pygame.draw.polygon(screen, (0,180,60), (p1, p2, p3), 0)
+            pygame.draw.polygon(screen, (0,180,60), (p1, p2, p3), 0)
 
 if __name__ == '__main__':
     SCREEN_WIDTH = 800
     SCREEN_HEIGHT = 800
     NUM_ROWS = 50
     NUM_COLS = 50
-    CELL_SIZE = (SCREEN_WIDTH/NUM_COLS, SCREEN_HEIGHT/NUM_ROWS)
+    CELL_SIZE = [SCREEN_WIDTH//NUM_COLS, SCREEN_HEIGHT//NUM_ROWS]
     
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.init()
+    screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+    pygame.display.set_caption('Python')
+    screen.fill((255,255,255))
     
     board = init_board(NUM_COLS, NUM_ROWS)
     
     #Draw the empty board
     for i in range(NUM_COLS):
         for j in range(NUM_ROWS):
-            pygame.draw.rect(screen, (255,255,255), (i*CELL_SIZE[0], j*CELL_SIZE[1], CELL_SIZE[0], CELL_SIZE[1]), 1)
+            pygame.draw.rect(screen, (0,0,0), (i*CELL_SIZE[0], j*CELL_SIZE[1], CELL_SIZE[0], CELL_SIZE[1]), 0)
             
     #Initialize Snake with random position
     player = Snake(random_pos(NUM_COLS, NUM_ROWS), random_direction())
@@ -142,3 +173,59 @@ if __name__ == '__main__':
     #Initialize food position
     food = random_pos(NUM_COLS, NUM_ROWS)
     pygame.draw.rect(screen, (180, 0, 0), (food[0]*CELL_SIZE[0], food[1]*CELL_SIZE[1], CELL_SIZE[0], CELL_SIZE[1]), 0)
+    
+    clock = pygame.time.Clock()
+    
+    playing = True
+    while playing:
+    
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                playing = False
+                
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    player.turn('left')
+                if event.key == pygame.K_RIGHT:
+                    player.turn('right')
+                if event.key == pygame.K_UP:
+                    player.turn('up')
+                if event.key == pygame.K_DOWN:
+                    player.turn('down') # for what
+        
+        step = player.step(food)
+        if step == -1:
+            #Failed, display score and prompt to play again or close
+            Tk().wm_withdraw()
+            
+            result = messagebox.askokcancel('Game over', ('Final Score '+str(len(player.body)-2)+'\n\nPlay again?'))
+            if result:
+                os.execl(sys.executable, sys.executable, *sys.argv)
+            else:
+                game_over = True
+                while game_over:
+                    ev = pygame.event.get()
+                    for event in ev:
+                        if event.type == pygame.KEYDOWN or event.type == pygame.QUIT:
+                            game_over = False
+                            break            
+            pygame.quit()
+        elif step == 1:
+            food = random_pos(NUM_COLS, NUM_ROWS)
+            while food in player.body or food == player.head:
+                food = random_pos(NUM_COLS, NUM_ROWS)
+            
+        for i in range(NUM_COLS):
+            for j in range(NUM_ROWS):
+                pygame.draw.rect(screen, (0,0,0), (i*CELL_SIZE[0], j*CELL_SIZE[1], CELL_SIZE[0], CELL_SIZE[1]), 0)
+                
+        
+        pygame.draw.rect(screen, (180, 0, 0), (food[0]*CELL_SIZE[0], food[1]*CELL_SIZE[1], CELL_SIZE[0], CELL_SIZE[1]), 0)
+        player.draw(screen)
+        
+        pygame.display.flip()
+        
+        #Speed up as snake grows
+        clock.tick(5+2*len(player.body))
+        
+    pygame.quit()
